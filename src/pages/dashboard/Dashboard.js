5d896c03e78b397aeec83363ee9ea524ef030950
GNeuro <dulaj.gneuro@gmail.com>
2024-12-01 14:01:57 +0530

    sample pr change

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,7 +1,10 @@
 const Dashboard = () => {
-    return (
-        <h1>Dashboard</h1>
-    )
-}
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>We Love GNeuro!!!</p>
+    </div>
+  );
+};
 
 export default Dashboard;
